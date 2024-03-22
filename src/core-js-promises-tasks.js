a92b37c76c6680a64f6062062cd2b9b3e283f0cb
Kirill <kiw197204@gmail.com>
2024-03-22 03:20:59 +0300

    feat: last unfinished

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -131,8 +131,12 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  let res = Promise.resolve('');
+  promises.forEach((promise) => {
+    res = res.then((resval) => promise.then((val) => val + resval));
+  });
+  return res;
 }
 
 module.exports = {
